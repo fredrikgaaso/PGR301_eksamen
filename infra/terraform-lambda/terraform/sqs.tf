034c104095b906df0b7e2023d5405d2f8372ddf2
fredrikgaaso <fredrikravndalgaaso@gmail.com>
2024-11-15 12:03:53 +0100

    trying ti puush with other branchQ

--- infra/terraform-lambda/terraform/sqs.tf
+++ infra/terraform-lambda/terraform/sqs.tf
@@ -1,6 +1,6 @@
 resource "aws_sqs_queue" "sqs_queue" {
   name                       = "lambda_sqs_queue_75"
-  visibility_timeout_seconds = 60
+  visibility_timeout_seconds = 55
   message_retention_seconds  = 1209600
   delay_seconds              = 10
   max_message_size           = 262144
